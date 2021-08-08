6f4df8815a7ed574930c7e67d3b91cc895997421
tonKristall <krisale@yandex.ru>
2021-08-08 17:22:14 +0300

    refactor: move components

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { CardsContainer } from "./components/cards/cards";
+import SearchBar from "./components/search-bar/search-bar";
 import './styles.scss';
 
 export function App():JSX.Element {
@@ -6,23 +8,4 @@ export function App():JSX.Element {
       <SearchBar />
       <CardsContainer />
     </div>
-}
-
-export function SearchBar():JSX.Element {
-  return <div className='search-bar'>
-    <div className='search-bar__wrapper'>
-      <input type='text' className='search-bar__input'></input>
-      <span className='search-bar__button'>search</span>
-    </div>
-  </div>
-}
-
-export function CardsContainer():JSX.Element {
-  return <div>
-    <Card />
-  </div>
-}
-
-export function Card():JSX.Element {
-  return <div>Tes React</div>
 }
---
+++ src/components/cards/cards.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './cards.scss';
+
+export function CardsContainer():JSX.Element {
+    return <div>
+      <Card />
+    </div>
+  }
+  
+  export function Card():JSX.Element {
+    return <div>Tes React</div>
+  }
