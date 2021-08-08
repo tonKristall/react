c97aa2c1284bbc4ead995698120d226b8ff840aa
tonKristall <krisale@yandex.ru>
2021-08-09 01:15:11 +0300

    refactor: rewrite component cards

--- src/components/cards/cards.tsx
+++ src/components/cards/cards.tsx
@@ -1,54 +1,53 @@
-import React from 'react';
-import './cards.scss';
+import React from "react";
+import "./cards.scss";
 
 const CARDS_DATA: Record<string, string>[] = [
   {
-    nameCards: 'Action (set A)',
-    imgCards: './img/cry.jpg',
+    nameCards: "Action (set A)",
+    imgCards: "./img/cry.jpg",
   },
   {
-    nameCards: 'Action (set B)',
-    imgCards: './img/open.jpg',
+    nameCards: "Action (set B)",
+    imgCards: "./img/open.jpg",
   },
   {
-    nameCards: 'Vegetables',
-    imgCards: './img/vegetables.jpg',
+    nameCards: "Vegetables",
+    imgCards: "./img/vegetables.jpg",
   },
   {
-    nameCards: 'Transport',
-    imgCards: './img/transport.jpg',
+    nameCards: "Transport",
+    imgCards: "./img/transport.jpg",
   },
   {
-    nameCards: 'Animal (set A)',
-    imgCards: './img/cat.jpg',
+    nameCards: "Animal (set A)",
+    imgCards: "./img/cat.jpg",
   },
   {
-    nameCards: 'Animal (set B)',
-    imgCards: './img/bird.jpg',
+    nameCards: "Animal (set B)",
+    imgCards: "./img/bird.jpg",
   },
   {
-    nameCards: 'Clothes',
-    imgCards: './img/skirt.jpg',
+    nameCards: "Clothes",
+    imgCards: "./img/skirt.jpg",
   },
   {
-    nameCards: 'Emotions',
-    imgCards: './img/sad.jpg',
+    nameCards: "Emotions",
+    imgCards: "./img/sad.jpg",
   },
 ];
 
-function Card(el: Record<string, string>): JSX.Element {
-  return (
-    <div className="card">
-      <img src={el.imgCards} alt={el.nameCards}></img>
-      <span>{el.nameCards}</span>
-    </div>
-  );
-}
-
 export default function CardsContainer(): JSX.Element {
+  const cardElement = CARDS_DATA.map((el) => {
+    return (
+      <div className="card">
+        <img src={el.imgCards} alt={el.nameCards}></img>
+        <span>{el.nameCards}</span>
+      </div>
+    );
+  });
   return (
     <div className="cards-container">
-      {CARDS_DATA.map((el) => Card(el))}
+      {cardElement}
     </div>
   );
 }
