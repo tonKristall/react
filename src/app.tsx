77f4818c3b626767fc91d78af817b8f462d61e4f
Aleksander Shustov <krisale@yandex.ru>
2021-08-23 20:38:31 +0300

    refactor: delete cards

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import CardsContainer from './components/cards/cards';
 import SearchBar from './components/search-bar/search-bar';
 import './styles.scss';
 
 export default function App():JSX.Element {
   return <div className='app-container'>
     <SearchBar />
-    <CardsContainer />
   </div>;
 }
+
+
+// Key:
+// 5ec1ed0a7188f2205fc06b771a57adb7
+
+// Secret:
+// fad83a26379e08e3
+
+// https://www.flickr.com/services/rest/?
+// text:
+// sort : date-posted-asc, date-posted-desc, date-taken-asc, date-taken-desc, interestingness-desc, interestingness-asc, relevance.
