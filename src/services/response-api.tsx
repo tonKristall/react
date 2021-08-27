1998d72f5332a879a3863d9a55d769980803e274
Aleksander Shustov <krisale@yandex.ru>
2021-08-28 00:01:50 +0300

    fix: error response

--- src/services/response-api.tsx
+++ src/services/response-api.tsx
@@ -1,11 +1,11 @@
-import { API_KEY, BASE_URL, SEARCH_DATA_DEFAULT } from '../const';
-import { IResultSearch, TSetStateArray, TSetStateSearchData } from '../types';
+import { API_KEY, BASE_URL, SEARCH_DATA_DEFAULT } from "../const";
+import { IResultSearch, TSetStateArray, TSetStateSearchData } from "../types";
 
 export default function ResponseAPI(
   setSearchData: TSetStateSearchData,
   searchData: typeof SEARCH_DATA_DEFAULT,
   setResultSearch: TSetStateArray,
-  resultSearch: IResultSearch,
+  resultSearch: IResultSearch
 ): void {
   setSearchData({ ...searchData, loading: !SEARCH_DATA_DEFAULT.loading });
   const searchValue = searchData.value;
@@ -17,22 +17,30 @@ export default function ResponseAPI(
   fetch(response)
     .then((resp) => resp.json())
     .then((resp: IResultSearch) => {
-      setResultSearch({
-        error: false,
-        articles: resp.articles,
-      });
+      if (resp.articles) {
+        setResultSearch({
+          error: false,
+          articles: resp.articles
+        });
+      } else {
+        setResultSearch({
+          error: true,
+          articles: resultSearch.articles
+        });
+      }
     })
     .catch(() => {
+      console.log(`error${resultSearch.articles}`);
       setResultSearch({
         error: true,
-        articles: resultSearch.articles,
+        articles: resultSearch.articles
       });
     })
     .finally(() => {
       setSearchData({
         ...searchData,
         loading: SEARCH_DATA_DEFAULT.loading,
-        requestRun: true,
+        requestRun: true
       });
     });
 }
